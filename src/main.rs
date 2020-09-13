f5b5a387366be713f8ef758f4521898072b49a8d
Yuichi Komorida <nicomathajax32@gmail.com>
2020-09-14 04:48:01 +0900

    Add ModP to main.rs etc.

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,45 @@ fn main() {
         mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
     }
 }
+
+use num_traits::{pow, One};
+use std::ops::{Add, Div, Mul, Sub};
+
+const MODULUS: usize = 1000000007;
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct ModP(usize);
+
+impl One for ModP {
+    fn one() -> Self {
+        return ModP(1);
+    }
+}
+impl Add for ModP {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        return ModP((self.0 + rhs.0) % MODULUS);
+    }
+}
+impl Sub for ModP {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        return ModP((self.0 + MODULUS - rhs.0) % MODULUS);
+    }
+}
+impl Mul for ModP {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        return ModP((self.0 * rhs.0) % MODULUS);
+    }
+}
+impl Div for ModP {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self {
+        if rhs.0 == 0 {
+            panic!("Tried to divide by ModP(0)!");
+        }
+        let rhs_inv = pow(rhs, MODULUS - 2);
+        return self * rhs_inv;
+    }
+}
